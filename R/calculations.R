709ddc2e840945662a53a32a5b32dbf40325bafb
Henk <henk.van.der.veen@gmail.com>
2020-01-22 23:31:56 +0100

    Made project into package and added vignette for report. can't get devtools::build_vignettes() to work

---
+++ R/calculations.R
@@ -0,0 +1,12 @@
+
+#' Absolute nonsense
+#'
+#' @return iris data.frame but with the width scaled by given factor.
+#' @export
+#'
+#' @examples rstuff::iris.scale.width(3)[1,1] == 1543 || stop("wrong")
+iris.scale.width <- function(scale) {
+  data(iris)
+  iris[1] <- iris[1] * 2
+  iris
+}
